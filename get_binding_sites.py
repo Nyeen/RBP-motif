8808c8e95d8e12aced969c51cc906672925d421e
Imrad Zulkar Nyeen <54758171+Nyeen@users.noreply.github.com>
2024-02-05 22:15:42 +1100

    Update and rename get_sequence_data.py to get_binding_sites.py

---
+++ get_binding_sites.py
@@ -0,0 +1,32 @@
+from Bio import SeqIO
+
+fasta_file = 'RIP-seq/hg19.fa'
+RBP_human = 'POSTAR3-peaks/human.txt'
+
+
+fr = open(RBP_human, 'r')
+fw = open('RBP_human.txt', 'w')
+
+chrom = 'chr'
+while True:
+    line = fr.readline()
+    
+    if not line:
+        break
+    
+    bs_info = line.split('\t')
+    
+    if bs_info[0] != chrom:
+        fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+        for fasta in fasta_sequences:
+            if fasta.id == bs_info[0]:
+                chrom = fasta.id
+                break
+    
+    sequence = str(fasta.seq).upper()[int(bs_info[1]):int(bs_info[2])]
+    fw.write(bs_info[0]+'\t'+bs_info[1]+'\t'+bs_info[2]+'\t'+sequence+'\t'+
+             bs_info[4]+'\t'+bs_info[5]+'\t'+bs_info[7]+'\n')
+
+fr.close()
+fw.close()
+
